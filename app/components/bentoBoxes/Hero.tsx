c337ce149c9d5655bc10e2a89ac469a506ea738d
Brody <brodypen7987@gmail.com>
2024-07-06 14:16:19 -0500

    add: finish header

--- app/components/bentoBoxes/Hero.tsx
+++ app/components/bentoBoxes/Hero.tsx
@@ -1,17 +1,25 @@
+import { LinkPreview } from "@/ui/LinkPreview";
 import RetroGrid from "@/ui/RetroGrid";
 import React from "react";
 
 const Hero = () => {
   return (
-    <div className="relative flex h-full w-full min-h-48 items-center justify-center overflow-hidden rounded-lg bg-background md:shadow-xl">
-      <span className="pointer-events-none z-10 whitespace-pre-wrap  text-center text-7xl font-bold leading-none tracking-tighter">
-        <span className="bg-gradient-to-b bg-clip-text">
-          👋 I&#39;m Brody{" "}
-          <span className="bg-gradient-to-b from-red-300 via-purple-300 to-blue-700 bg-clip-text text-transparent">
-            !
+    <div className="relative flex h-full w-full min-h-48 items-center justify-center overflow-hidden rounded-lg bg-background md:shadow-xl z-50">
+      <LinkPreview
+        url="https://www.linkedin.com/in/brodypen/"
+        imageSrc="/Brody_Pen_Headshot.png"
+        isStatic
+        className="font-bold z-[1000]"
+      >
+        <span className="z-60 whitespace-pre-wrap  text-center text-7xl font-bold leading-none tracking-tighter">
+          <span className="bg-gradient-to-b bg-clip-text">
+            👋 I&#39;m Brody{" "}
+            <span className="bg-gradient-to-b from-red-300 via-purple-300 to-blue-700 bg-clip-text text-transparent">
+              !
+            </span>
           </span>
         </span>
-      </span>
+      </LinkPreview>
 
       <RetroGrid />
     </div>
