d5db4323302e0b7b93f7599cc791fade1e9226d7
Brody <brodypen7987@gmail.com>
2024-07-06 20:34:50 -0500

    add: marquee flavor

--- app/components/bentoBoxes/MarqueeBox.tsx
+++ app/components/bentoBoxes/MarqueeBox.tsx
@@ -1,91 +1,95 @@
 import { cn } from "@/lib/utils";
 import Marquee from "@/ui/Marquee";
+import Image from "next/image";
 
 // bg-red-500
 const experiences = [
   {
     name: "JPMorganChase",
-    username: "SWE Summer Intern 2025",
-    body: "Worked and did things. it was great to do this work.",
-    img: "https://avatar.vercel.sh/jack",
+    username: "SWE Summer Intern 2024",
+    body: "Developed solutions to support financial services and operations. ",
+    img: "/marqueeAssets/JPMorganChase.jpeg",
+    className: "dark:bg-orange-500/[.25]",
+  },
+  {
+    name: "CougarCS",
+    username: "Web Master",
+    body: "Led web projects development at the biggest CS org in UH",
+    img: "/marqueeAssets/cougarCS_WebDev.jpeg",
+    className: "dark:bg-pink-500/[.25]",
+  },
+  {
+    name: "University of Houston",
+    username: "ConocoPhilips Center Tutor",
+    body: "Improved CS Students academic performance.",
+    img: "/marqueeAssets/UHNSM_Tutor.jpeg",
+    className: "dark:bg-rose-500/[.25]",
   },
 ];
 // bg-blue-500
 const projects = [{}];
 // bg-purple-500
 const reviews = [
+  // {
+  //   name: "Ashish Job",
+  //   username: "SRE @ JPMC, SWE @ NASA",
+  //   body: "I've never seen anything like this before. It's amazing. I love it.",
+  //   img: "https://avatar.vercel.sh/jack",
+  // },
   {
-    name: "Ashish Job",
-    username: "SRE @ JPMC, SWE @ NASA",
-    body: "I've never seen anything like this before. It's amazing. I love it.",
-    img: "https://avatar.vercel.sh/jack",
+    name: "Jorell Padilla",
+    username: "UX Design Intern @ PwC",
+    body: "I always trust Brody to turn my designs into something amazing!",
+    img: "/marqueeAssets/JorellP.jpeg",
+    className: "dark:bg-purple-500/[.25]",
   },
-  {
-    name: "Jill",
-    username: "@jill",
-    body: "I don't know what to say. I'm speechless. This is amazing.",
-    img: "https://avatar.vercel.sh/jill",
-  },
-  {
-    name: "John",
-    username: "@john",
-    body: "I'm at a loss for words. This is amazing. I love it.",
-    img: "https://avatar.vercel.sh/john",
-  },
-  {
-    name: "Jane",
-    username: "@jane",
-    body: "I'm at a loss for words. This is amazing. I love it.",
-    img: "https://avatar.vercel.sh/jane",
-  },
-  {
-    name: "Jenny",
-    username: "@jenny",
-    body: "I'm at a loss for words. This is amazing. I love it.",
-    img: "https://avatar.vercel.sh/jenny",
-  },
-  {
-    name: "James",
-    username: "@james",
-    body: "I'm at a loss for words. This is amazing. I love it.",
-    img: "https://avatar.vercel.sh/james",
-  },
+  // {
+  //   name: "John",
+  //   username: "@john",
+  //   body: "I'm at a loss for words. This is amazing. I love it.",
+  //   img: "https://avatar.vercel.sh/john",
+  // },
+  // {
+  //   name: "Jane",
+  //   username: "@jane",
+  //   body: "I'm at a loss for words. This is amazing. I love it.",
+  //   img: "https://avatar.vercel.sh/jane",
+  // },
+  // {
+  //   name: "Jenny",
+  //   username: "@jenny",
+  //   body: "I'm at a loss for words. This is amazing. I love it.",
+  //   img: "https://avatar.vercel.sh/jenny",
+  // },
+  // {
+  //   name: "James",
+  //   username: "@james",
+  //   body: "I'm at a loss for words. This is amazing. I love it.",
+  //   img: "https://avatar.vercel.sh/james",
+  // },
 ];
 
-// const firstRow = reviews.slice(0, reviews.length / 2);
-const secondRow = reviews.slice(reviews.length / 2);
-const firstRow = experiences;
-const ExperienceCard = ({
-  img,
-  name,
-  username,
-  body,
-}: {
-  img: string;
-  name: string;
-  username: string;
-  body: string;
-}) => {
+const data = experiences.concat(reviews);
+const firstRow = data.slice(0, data.length / 2);
+const secondRow = data.slice(data.length / 2);
+const ProjectCard = ({ img }: { img: string }) => {
   return (
     <figure
       className={cn(
-        "relative w-64 cursor-pointer overflow-hidden rounded-xl border p-4",
+        "relative w-64 h-28 cursor-pointer overflow-hidden rounded-xl border",
         // light styles
         "border-gray-950/[.1] bg-gray-950/[.01] hover:bg-gray-950/[.05]",
         // dark styles
-        "dark:border-gray-50/[.1] dark:bg-gray-50/[.10] dark:hover:bg-gray-50/[.15]"
+        "dark:border-gray-50/[.1] dark:bg-red-500/[.25] dark:hover:bg-gray-50/[.15]"
       )}
     >
-      <div className="flex flex-row items-center gap-2">
-        <img className="rounded-full" width="32" height="32" alt="" src={img} />
-        <div className="flex flex-col">
-          <figcaption className="text-sm font-medium dark:text-white">
-            {name}
-          </figcaption>
-          <p className="text-xs font-medium dark:text-white/40">{username}</p>
-        </div>
-      </div>
-      <blockquote className="mt-2 text-sm">{body}</blockquote>
+      <Image
+        className="h-full w-full"
+        width="32"
+        height="32"
+        alt=""
+        src={img}
+      />
     </figure>
   );
 };
@@ -95,24 +99,34 @@ const ReviewCard = ({
   name,
   username,
   body,
+  className,
 }: {
   img: string;
   name: string;
   username: string;
   body: string;
+  className?: string;
 }) => {
+  console.log(className);
   return (
     <figure
       className={cn(
-        "relative w-64 cursor-pointer overflow-hidden rounded-xl border p-4",
+        "relative w-64 h-28 cursor-pointer overflow-hidden rounded-xl border p-4",
         // light styles
         "border-gray-950/[.1] bg-gray-950/[.01] hover:bg-gray-950/[.05]",
         // dark styles
-        "dark:border-gray-50/[.1] dark:bg-red-500/[.25] dark:hover:bg-gray-50/[.15]"
+        "dark:border-gray-50/[.1] dark:bg-red-500/[.25] dark:hover:bg-gray-50/[.05]",
+        className
       )}
     >
       <div className="flex flex-row items-center gap-2">
-        <img className="rounded-full" width="32" height="32" alt="" src={img} />
+        <Image
+          className="rounded-full"
+          width="32"
+          height="32"
+          alt=""
+          src={img}
+        />
         <div className="flex flex-col">
           <figcaption className="text-sm font-medium dark:text-white">
             {name}
@@ -120,7 +134,7 @@ const ReviewCard = ({
           <p className="text-xs font-medium dark:text-white/40">{username}</p>
         </div>
       </div>
-      <blockquote className="mt-2 text-sm">{body}</blockquote>
+      <blockquote className="mt-2 text-sm line-clamp-2">{body}</blockquote>
     </figure>
   );
 };
