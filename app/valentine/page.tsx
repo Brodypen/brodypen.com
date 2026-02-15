b5e1f86b6c76b6bfe1e78d5360ad96211dc98a1f
Brodypen <brodypen7987@gmail.com>
2026-02-14 17:15:54 -0800

    quick change

--- app/valentine/page.tsx
+++ app/valentine/page.tsx
@@ -17,49 +17,56 @@ export default function ValentinePage() {
 
   const ScrapbookContent = () => {
     const items = [
-      // Page 1
+      // Section 1
       { type: "h2", text: "Hi Honey! :)", style: { top: "5rem", left: "5rem", transform: "rotate(-6deg)", fontSize: "3.5rem" }, font: "font-patrick-hand", color: "text-pink-300" },
       { type: "p", text: "Wow! It's been another year together huh?", style: { top: "15rem", right: "5rem", width: "400px", transform: "rotate(2deg)" }, font: "font-caveat", color: "text-white", size: "text-3xl" },
       { type: "img", src: "/photos-valenties/houston.jpeg", alt: "Houston", style: { top: "25rem", left: "10rem", width: "300px", height: "370px", transform: "rotate(5deg)" } },
 
-      // Page 2
-      { type: "p", text: "I'm so excited to be able to celebrate...", style: { top: "55rem", left: "2rem", width: "400px", transform: "rotate(-3deg)" }, font: "font-caveat", color: "text-white", size: "text-3xl" },
-      { type: "p", text: "...this Valentine's day with you!", style: { top: "60rem", right: "2rem", width: "350px", transform: "rotate(4deg)" }, font: "font-caveat", color: "text-white", size: "text-3xl" },
-      
-      // Original New text section
-      { type: "p", text: "I'm so thankful to be with you :)", style: { top: "70rem", left: "4rem", width: "400px", transform: "rotate(-2deg)" }, font: "font-caveat", color: "text-green-300", size: "text-3xl" },
-      { type: "img", src: "/photos-valenties/image copy 5.png", alt: "Image 5", style: { top: "78rem", right: "2rem", width: "300px", height: "230px", transform: "rotate(5deg)" } },
-      { type: "p", text: "I'm happy to talk to you and learn so much about you!", style: { top: "95rem", right: "1rem", width: "450px", transform: "rotate(3deg)" }, font: "font-caveat", color: "text-white", size: "text-3xl" },
-      { type: "img", src: "/photos-valenties/ferris-wheel.jpeg", alt: "Ferris Wheel", style: { top: "105rem", left: "5rem", width: "320px", height: "250px", transform: "rotate(6deg)" } },
-      { type: "p", text: "I'm happy to be with you for another year hehe... and many more!", style: { top: "128rem", left: "2rem", width: "500px", transform: "rotate(-3deg)" }, font: "font-caveat", color: "text-white", size: "text-3xl" },
-      { type: "img", src: "/photos-valenties/image copy.png", alt: "Image Copy", style: { top: "140rem", right: "2rem", width: "290px", height: "230px", transform: "rotate(-4deg)" } },
-      { type: "p", text: "I'm so excited for our future and can't wait", style: { top: "155rem", left: "3rem", width: "500px", transform: "rotate(2deg)" }, font: "font-caveat", color: "text-blue-300", size: "text-3xl" },
+      // Section 2
+      { type: "p", text: "I'm so excited to celebrate this Valentine's day with you!", style: { top: "55rem", left: "2rem", width: "450px", transform: "rotate(-3deg)" }, font: "font-caveat", color: "text-white", size: "text-3xl" },
+      { type: "img", src: "/photos-valenties/image copy 5.png", alt: "Image 5", style: { top: "65rem", right: "2rem", width: "300px", height: "230px", transform: "rotate(5deg)" } },
 
-      // USER'S LATEST ADDITIONS
-      { type: "p", text: "to see you in person soon :)!!", style: { top: "160rem", right: "3rem", width: "450px", transform: "rotate(-3deg)" }, font: "font-caveat", color: "text-blue-300", size: "text-3xl" },
-      { type: "p", text: "Anyway back to our Valentie's day date :)", style: { top: "170rem", left: "4rem", width: "500px", transform: "rotate(1deg)" }, font: "font-patrick-hand", color: "text-pink-300", size: "text-3xl" },
-
-      // Pushed-down content
-      { type: "img", src: "/photos-valenties/image copy 2.png", alt: "Image 2", style: { top: "185rem", left: "3rem", width: "280px", height: "210px", transform: "rotate(-5deg)" } },
-      { type: "p", text: "I hope you got your yummy food...", style: { top: "200rem", right: "3rem", width: "380px", transform: "rotate(-4deg)" }, font: "font-caveat", color: "text-white", size: "text-3xl" },
-      { type: "p", text: "...and are ready for a night of fun and games.", style: { top: "208rem", left: "2rem", width: "450px", transform: "rotate(1deg)" }, font: "font-caveat", color: "text-white", size: "text-3xl" },
-      { type: "img", src: "/photos-valenties/gas-station.jpeg", alt: "Gas Station", style: { top: "220rem", right: "2rem", width: "300px", height: "220px", transform: "rotate(-3deg)" } },
+      // Section 3
+      { type: "p", text: "I'm so thankful to be with you :)", style: { top: "85rem", left: "4rem", width: "400px", transform: "rotate(-2deg)" }, font: "font-caveat", color: "text-green-300", size: "text-3xl" },
+      { type: "img", src: "/photos-valenties/ferris-wheel.jpeg", alt: "Ferris Wheel", style: { top: "95rem", left: "5rem", width: "320px", height: "250px", transform: "rotate(6deg)" } },
       
-      { type: "p", text: "This will be a night to remember :)", style: { top: "240rem", left: "2rem", width: "400px", transform: "rotate(5deg)" }, font: "font-caveat", color: "text-white", size: "text-3xl" },
-      { type: "img", src: "/photos-valenties/image.png", alt: "Image", style: { top: "250rem", right: "3rem", width: "260px", height: "200px", transform: "rotate(3deg)" } },
-      { type: "img", src: "/photos-valenties/eating-food.jpeg", alt: "Eating Food", style: { top: "265rem", left: "8rem", width: "250px", height: "300px", transform: "rotate(-8deg)" } },
+      // Section 4
+      { type: "p", text: "I'm happy to talk to you and learn so much about you!", style: { top: "125rem", right: "1rem", width: "450px", transform: "rotate(3deg)" }, font: "font-caveat", color: "text-white", size: "text-3xl" },
+      { type: "img", src: "/photos-valenties/image copy.png", alt: "Image Copy", style: { top: "135rem", right: "2rem", width: "290px", height: "230px", transform: "rotate(-4deg)" } },
 
-      { type: "p", text: "I love you Honey.", style: { top: "300rem", left: "10rem", width: "300px", transform: "rotate(3deg)" }, font: "font-patrick-hand", color: "text-red-300", size: "text-5xl" },
-      { type: "img", src: "/photos-valenties/birthday-2025.jpeg", alt: "Birthday 2025", style: { top: "310rem", right: "3rem", width: "280px", height: "210px", transform: "rotate(-5deg)" } },
-      { type: "img", src: "/photos-valenties/image copy 3.png", alt: "Image 3", style: { top: "330rem", left: "4rem", width: "290px", height: "220px", transform: "rotate(7deg)" } },
-      
-      { type: "img", src: "/photos-valenties/image copy 4.png", alt: "Image 4", style: { top: "355rem", right: "5rem", width: "260px", height: "200px", transform: "rotate(-2deg)" } },
-      { type: "img", src: "/photos-valenties/image copy 6.png", alt: "Image 6", style: { top: "375rem", left: "3rem", width: "270px", height: "210px", transform: "rotate(-6deg)" } },
-      { type: "img", src: "/photos-valenties/image copy 7.png", alt: "Image 7", style: { top: "395rem", right: "1rem", width: "280px", height: "220px", transform: "rotate(8deg)" } },
+      // Section 5
+      { type: "p", text: "I'm happy to be with you for another year hehe... and many more!", style: { top: "155rem", left: "2rem", width: "500px", transform: "rotate(-3deg)" }, font: "font-caveat", color: "text-white", size: "text-3xl" },
+      { type: "img", src: "/photos-valenties/image copy 2.png", alt: "Image 2", style: { top: "165rem", left: "3rem", width: "280px", height: "210px", transform: "rotate(-5deg)" } },
+
+      // Section 6
+      { type: "p", text: "I'm so excited for our future and can't wait to see you in person soon :)!!", style: { top: "185rem", left: "3rem", width: "500px", transform: "rotate(2deg)" }, font: "font-caveat", color: "text-blue-300", size: "text-3xl" },
+      { type: "img", src: "/photos-valenties/gas-station.jpeg", alt: "Gas Station", style: { top: "195rem", right: "2rem", width: "300px", height: "220px", transform: "rotate(-3deg)" } },
+
+      // New Section
+      { type: "p", text: "Someone said that when I see something I like I grab it", style: { top: "220rem", left: "2rem", width: "480px", transform: "rotate(-2deg)" }, font: "font-caveat", color: "text-white", size: "text-3xl" },
+      { type: "p", text: "So I'm so happy to be grabbed by you! hehe...", style: { top: "225rem", right: "2rem", width: "400px", transform: "rotate(3deg)" }, font: "font-caveat", color: "text-yellow-300", size: "text-3xl" },
+      { type: "img", src: "/photos-valenties/image copy 4.png", alt: "Image 4", style: { top: "235rem", left: "5rem", width: "260px", height: "200px", transform: "rotate(-2deg)" } },
+
+      // Section 7 (Pushed down)
+      { type: "p", text: "Anyway back to our Valentie's day date :)", style: { top: "255rem", left: "4rem", width: "500px", transform: "rotate(1deg)" }, font: "font-patrick-hand", color: "text-pink-300", size: "text-3xl" },
+      { type: "img", src: "/photos-valenties/image.png", alt: "Image", style: { top: "265rem", right: "3rem", width: "260px", height: "200px", transform: "rotate(3deg)" } },
+
+      // Section 8 (Pushed down)
+      { type: "p", text: "I hope you got your yummy food and are ready for a night of fun and games.", style: { top: "285rem", left: "2rem", width: "450px", transform: "rotate(1deg)" }, font: "font-caveat", color: "text-white", size: "text-3xl" },
+      { type: "img", src: "/photos-valenties/eating-food.jpeg", alt: "Eating Food", style: { top: "295rem", left: "8rem", width: "250px", height: "300px", transform: "rotate(-8deg)" } },
+
+      // Section 9 (Pushed down)
+      { type: "p", text: "This will be a night to remember :)", style: { top: "330rem", right: "2rem", width: "400px", transform: "rotate(5deg)" }, font: "font-caveat", color: "text-white", size: "text-3xl" },
+      { type: "img", src: "/photos-valenties/birthday-2025.jpeg", alt: "Birthday 2025", style: { top: "340rem", right: "3rem", width: "280px", height: "210px", transform: "rotate(-5deg)" } },
+
+      // Section 10 (Pushed down)
+      { type: "p", text: "I love you Honey.", style: { top: "365rem", left: "10rem", width: "300px", transform: "rotate(3deg)" }, font: "font-patrick-hand", color: "text-red-300", size: "text-5xl" },
+      { type: "img", src: "/photos-valenties/image copy 3.png", alt: "Image 3", style: { top: "375rem", left: "4rem", width: "290px", height: "220px", transform: "rotate(7deg)" } },
+      { type: "img", src: "/photos-valenties/image copy 7.png", alt: "Image 7", style: { top: "400rem", right: "1rem", width: "280px", height: "220px", transform: "rotate(8deg)" } },
     ];
 
     return (
-        <div className="w-full h-full relative p-8" style={{ minHeight: "420rem" }}> 
+        <div className="w-full h-full relative p-8" style={{ minHeight: "450rem" }}> 
             {items.map((item, index) => {
                 const style = { ...item.style, animationDelay: `${index * 0.1}s` };
                 if (item.type === "img") {
